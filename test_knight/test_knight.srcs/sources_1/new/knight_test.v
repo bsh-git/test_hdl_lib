c68394ab81364f029084a73ce18dff716cddf67d
Hiroyuki Bessho <bessho@genetec.co.jp>
2018-01-09 12:19:27 +0900

    Vivado project to test knight rider module. vivado -mode batch -source project_test_knight.tcl

---
+++ test_knight/test_knight.srcs/sources_1/new/knight_test.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/01/08 23:01:06
+// Design Name: 
+// Module Name: knight_test
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module knight_test #
+  (
+   parameter integer WIDTH = 8,
+   parameter integer DIV = 24'hffffff
+   )
+   (
+    input 	       clk_in,
+    input 	       speed, 
+    output [WIDTH-1:0] led
+    );
+
+   wire 	       clk;
+   wire [23:0] 	       div;
+
+
+// fixed_divider #(DIV, 24) divider(.clk_in(clk_in), .clk_out(clk));
+   assign div = speed  ? DIV >> 1 : DIV;
+
+   variable_divider #(24) divider(.clk_in(clk_in), .clk_out(clk), .load(div));
+   
+   knight #(WIDTH) knight(.clk_in(clk), .led(led));
+   
+
+endmodule
