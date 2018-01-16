f29472cdd23e1d78d879695a815b5adbf02d98ea
Hiroyuki Bessho <bsh@binchodeb.genetec.local>
2018-01-16 17:00:18 +0900

    add a module to perform knight rider display using PWM on LEDs.

--- test_knight/srcs/knight_test.v
+++ test_knight/srcs/knight_test.v
@@ -20,26 +20,39 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
+`define TEST_PWM
+
 module knight_test #
   (
-   parameter integer WIDTH = 8,
-   parameter integer DIV = 24'hffffff
+   parameter integer N_LEDS = 8,
+   parameter integer DIV = 16000000 - 1
    )
    (
     input 	       clk_in,
     input 	       speed, 
-    output [WIDTH-1:0] led
+    output [N_LEDS-1:0] led
     );
 
    wire 	       clk;
    wire [23:0] 	       div;
 
 
+`ifndef TEST_PWM
    assign div = speed  ? DIV >> 1 : DIV;
 
    divider #(24) divider(.clk_in(clk_in), .clk_out(clk), .load(div));
    
-   knight #(WIDTH) knight(.clk_in(clk), .led(led));
-   
+   knight #(N_LEDS) knight(.clk_in(clk), .led(led));
+`else
+
+   (* mark_debug = "TRUE" *) wire  pwmclk, dbgclk, waveclk;
+
+   divider #(16) divdbg(.clk_in(clk_in), .load(2000-1), .clk_out(dbgclk));
+   divider #(16) div0(.clk_in(clk_in), .load(8000-1), .clk_out(pwmclk));
+   divider #(8) div1(.clk_in(pwmclk), .load(40-1), .clk_out(waveclk));
+
+   knight_pwm #(N_LEDS) knight(.pwm_clk(pwmclk), .mod_clk(waveclk), .led(led));
+
+`endif
 
 endmodule
